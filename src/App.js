a5f01670973c71a950a21ad82f9c96b39f30159a
Sergey Davydov <dev@sdcollection.com>
2022-09-03 13:40:57 +0300

    check locally for hashed link works from https://codepen.io/swed/pen/OJZJzBL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from "react-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="tabs">
+        <h1>React Tabs</h1>
+        Incoming window.location.hash <code>
+        <a href="{window.location.hash}">{window.location.hash}</a>
+        </code>
+        <Tabs>
+          <Tab label="Tab 1">
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0xIAJciTxzjZZM-viJfHk_Ezlv0nn8fft3ORQ9UkoP2mPd5pm"/>
+              <p>Tab 1 content</p>
+            </div>
+          </Tab>
+          <Tab label="Tab 2">
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2E3SrDqYWF9J_TDWNzjZkwJOAGxkLJVZPvUly7SqUL9WqLqEw"/>
+              <p>Tab 2 content</p>
+            </div>
+          </Tab>
+          <Tab label="Tab 3">
+            <div>
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB7Y713wstirF-PvKJa6OioS83ozc-BeFAzjjyndv58ZnfyaZK0g"/>
+              <p>Tab 3 content</p>
+            </div>
+          </Tab>
+          <Tab label="Tab 4">
+            <div>
+              <img src="http://choseneye.org/image/cache/catalog/SERVICES4-600x600.png"/>
+
+              <p>Tab 4 content</p>
+            </div>
+          </Tab>
+          <Tab label="Tab 5">
+            <div>
+              <img src="https://iconcept.com.my/wp-content/uploads/2018/10/iStock-956508212-600x600.jpg"/>
+              <p>Tab 5 content</p>
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+
+    )
+  }
 }
 
-export default App;
+class Tabs extends React.Component {
+  state = {
+    activeTab: this.props.children[0].props.label
+  }
+  changeTab = (tab) => {
+    this.setState({activeTab: tab});
+    window.location.hash = tab
+  };
+
+  render() {
+
+    let content;
+    let currentTab = decodeURI(window.location.hash.replace(/^#/, ''));
+    let buttons = [];
+
+    if (currentTab) {
+      this.state.activeTab = currentTab
+    }
+
+    return (
+      <div>
+        {React.Children.map(this.props.children, child => {
+          buttons.push(child.props.label)
+          if (child.props.label === this.state.activeTab) content = child.props.children
+        })}
+
+        <TabButtons activeTab={this.state.activeTab} buttons={buttons} changeTab={this.changeTab}/>
+        <div className="tab-content">{content}</div>
+
+      </div>
+    );
+  }
+}
+
+const TabButtons = ({buttons, changeTab, activeTab}) => {
+
+  return (
+    <div className="tab-buttons">
+      {buttons.map(button => {
+        return <button className={button === activeTab ? 'active' : ''}
+                       onClick={() => changeTab(button)}>{button}</button>
+      })}
+    </div>
+  )
+}
+
+const Tab = props => {
+  return (
+    <React.Fragment>
+      {props.children}
+    </React.Fragment>
+  )
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
