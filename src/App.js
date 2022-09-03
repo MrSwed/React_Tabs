8a9d708503aa67a8461d5a2a43c201f5b3641393
Sergey Davydov <dev@sdcollection.com>
2022-09-03 15:02:20 +0300

    variant from https://codepen.io/brettsnaidero/pen/RwwjKvm

--- src/App.js
+++ src/App.js
@@ -1,111 +1,96 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import { HashRouter, BrowserRouter as Routes, Route, Redirect, Link, matchPath, withRouter } from "react-router-dom";
 
-import './App.css';
+const Tabs = withRouter(({ location, items, match }) => {
+  const active = items.find((item) =>
+    matchPath(location.pathname, {
+      path: `/${item.route}`,
+      exact: true,
+    }),
+  );
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="tabs">
-        <h1>React Tabs</h1>
-        Incoming window.location.hash <code>
-        <a href="{window.location.hash}">{window.location.hash}</a>
-        </code>
-        <Tabs>
-          <Tab label="Tab 1">
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0xIAJciTxzjZZM-viJfHk_Ezlv0nn8fft3ORQ9UkoP2mPd5pm"/>
-              <p>Tab 1 content</p>
-            </div>
-          </Tab>
-          <Tab label="Tab 2">
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2E3SrDqYWF9J_TDWNzjZkwJOAGxkLJVZPvUly7SqUL9WqLqEw"/>
-              <p>Tab 2 content</p>
-            </div>
-          </Tab>
-          <Tab label="Tab 3">
-            <div>
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB7Y713wstirF-PvKJa6OioS83ozc-BeFAzjjyndv58ZnfyaZK0g"/>
-              <p>Tab 3 content</p>
-            </div>
-          </Tab>
-          <Tab label="Tab 4">
-            <div>
-              <img src="http://choseneye.org/image/cache/catalog/SERVICES4-600x600.png"/>
-
-              <p>Tab 4 content</p>
-            </div>
-          </Tab>
-          <Tab label="Tab 5">
-            <div>
-              <img src="https://iconcept.com.my/wp-content/uploads/2018/10/iStock-956508212-600x600.jpg"/>
-              <p>Tab 5 content</p>
-            </div>
-          </Tab>
-        </Tabs>
-      </div>
-
-    )
-  }
-}
-
-class Tabs extends React.Component {
-  state = {
-    activeTab: this.props.children[0].props.label
-  }
-  changeTab = (tab) => {
-    this.setState({activeTab: tab});
-    window.location.hash = tab
-  };
-
-  render() {
-
-    let content;
-    let currentTab = decodeURI(window.location.hash.replace(/^#/, ''));
-    let buttons = [];
-
-    if (currentTab) {
-      this.state.activeTab = currentTab
-    }
-
-    return (
-      <div>
-        {React.Children.map(this.props.children, child => {
-          buttons.push(child.props.label)
-          if (child.props.label === this.state.activeTab) content = child.props.children
-        })}
-
-        <TabButtons activeTab={this.state.activeTab} buttons={buttons} changeTab={this.changeTab}/>
-        <div className="tab-content">{content}</div>
-
-      </div>
-    );
-  }
-}
-
-const TabButtons = ({buttons, changeTab, activeTab}) => {
+  const activeId = active && active.id;
 
   return (
-    <div className="tab-buttons">
-      {buttons.map(button => {
-        return <button className={button === activeTab ? 'active' : ''}
-                       onClick={() => changeTab(button)}>{button}</button>
-      })}
+    <div className="tabs">
+      <ul role="tablist" aria-orientation="horizontal" className="tabs-list">
+        {/* We loop through the "items" and create tabs for each */}
+        {items.map((item) => (
+          <li className="tabs-list__item" key={`tab-${item.name}`}>
+            <Link
+              to={item.route}
+              className={`tabs-list__tab ${activeId === item.id ? 'active' : 'inactive'}`}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {/* Tab content - we use a Switch to only show one tab at a time */}
+      <Routes>
+        {/* We loop through the "items" and create routes to correspond to each tab */}
+        {items.map((item) => (
+          <Route
+            key={item.id}
+            path={`/${item.route}`}
+            render={item.render}
+
+          />
+        ))}
+        {/* We can have a Redirect route to ensure we always have an active route */}
+        <Route render={() => <Redirect to={items[0] ? items[0].route : '/'} />} />
+      </Routes>
     </div>
   )
-}
+});
 
-const Tab = props => {
+function App() {
   return (
-    <React.Fragment>
-      {props.children}
-    </React.Fragment>
-  )
+    <HashRouter>
+      <Tabs items={[
+        {
+          id: 'id1',
+          name: 'Tab #1',
+          route: 'id1',
+          render: () => (
+            <div className="tab-content">
+              Depths burying snare value law merciful value snare society eternal-return decieve aversion.
+              Holiest virtues pious war depths noble inexpedient against endless ultimate. Merciful
+              disgust convictions grandeur abstract battle gains revaluation fearful inexpedient right
+              holiest faithful battle. Merciful depths decrepit intentions virtues salvation war ultimate.
+              Sea transvaluation virtues suicide battle against victorious.
+            </div>
+          )
+        },
+        {
+          id: 'id2',
+          name: 'Tab #2',
+          route: 'id2',
+          render: () => (
+            <div className="tab-content">
+              Ideal overcome free burying grandeur aversion. Dead morality self right superiority passion
+              virtues hope society play of snare grandeur. Good oneself burying law good ultimate burying.
+              Play justice snare holiest noble sea reason marvelous right.
+            </div>
+          )
+        },
+        {
+          id: 'id3',
+          name: 'Tab #3',
+          route: 'id3',
+          render: () => (
+            <div className="tab-content">
+              Inexpedient gains prejudice aversion pious snare noble ocean ocean overcome self ubermensch
+              prejudice philosophy. Ocean strong sea burying reason ultimate burying spirit. Pious christianity
+              decieve endless abstract decrepit abstract. Ocean burying depths evil horror suicide mountains
+              fearful depths christianity disgust gains horror. Self marvelous passion faith against grandeur.
+            </div>
+          )
+        }
+      ]} />
+    </HashRouter>
+  );
 }
 
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
